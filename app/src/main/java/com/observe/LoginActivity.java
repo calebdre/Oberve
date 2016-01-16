abd82d87c54bbd94c3bc0301883d7c0a3a8f5836
Caleb Lewis <caleb_dre@yahoo.com>
2016-01-16 04:56:26 -0500

    merge

--- app/src/main/java/com/observe/LoginActivity.java
+++ app/src/main/java/com/observe/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.observe;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.play_btn) View playBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.play_btn)
+    public void onPlayButtonClick(View v){
+        TransitionDrawable transition = (TransitionDrawable) v.getBackground();
+        transition.startTransition(1000);
     }
 
     public void onClickLogin(View view) {
@@ -24,10 +37,5 @@ public class LoginActivity extends AppCompatActivity {
                         +"\n" + "Password: " + passWord.getText().toString());
 
         Toast.makeText(LoginActivity.this, "Logging in . . .", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onClickSignUp(View view) {
-        Intent intent = new Intent(this, signup_activity.class);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/observe/Views/VideoView.java
@@ -0,0 +1,15 @@
+package com.observe.Views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.observe.R;
+
+public class VideoView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
