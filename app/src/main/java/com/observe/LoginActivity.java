9d2da73d70ce22b646a7ece89926a8bd525356a8
lcheniv <the_crimson_chen@yahoo.com>
2016-01-16 04:48:58 -0500

    Here you go
    created the user name pass word text fields
    - also buttons for them
    - added image view > to use the layout / background

--- app/src/main/java/com/observe/LoginActivity.java
+++ app/src/main/java/com/observe/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.observe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickLogin(View view) {
+        EditText userName = (EditText) findViewById(R.id.username);
+        EditText passWord = (EditText) findViewById(R.id.password);
+
+        Log.i("Login information: ", "Username: " + userName.getText().toString()
+                        +"\n" + "Password: " + passWord.getText().toString());
+
+        Toast.makeText(LoginActivity.this, "Logging in . . .", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSignUp(View view) {
+        Intent intent = new Intent(this, signup_activity.class);
+        startActivity(intent);
     }
 }
